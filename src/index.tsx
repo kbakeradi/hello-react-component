da3e5485aa43c45e91672c72c3ca64b0d1437877
Baker, Kevin <Kevin.Baker@analog.com>
2022-03-30 11:41:50 -0400

    changed output of component

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,15 @@
 import * as React from 'react'
 import './styles.scss'
-// import styled from 'styled-components';
 
 type HelloProps = {
   name: string;
 }
 
-// const Component = styled.section`
-//   padding: 4em;
-//   background: papayawhip;
-// `;
 
 
 const HelloComponent:React.FC<HelloProps> = ({ name }) => {
   // name is string!
-  return <div className='make-big'>Hello {name}</div>
+  return <div className='make-big'>HelloComponent: "Hello {name}"</div>
 };
 
 export default HelloComponent;
